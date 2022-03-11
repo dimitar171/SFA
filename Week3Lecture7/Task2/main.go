9858a104e1a4fe7db5cf07438e300a8d31431538
Dime <dime.jordanov@gmail.com>
2022-03-11 14:03:42 +0100

    update 03.11.2022

--- Week3Lecture7/Task2/main.go
+++ Week3Lecture7/Task2/main.go
@@ -42,23 +42,23 @@ func maxCard(cards []Cards, comparatorFunc CardComparator) Cards {
 }
 
 //func with anonymous as a param
-func maxA(cards []Cards, compFunc func(Card1 Cards, Card2 Cards) int) Cards {
-	pom := cards[0]
-	for i := 1; i < 10; i++ {
-		s := compFunc(pom, cards[i])
-		if s == -1 {
-			pom = cards[i]
-		}
-	}
-	return pom
-}
+// func maxA(cards []Cards, compFunc func(Card1 Cards, Card2 Cards) int) Cards {
+// 	pom := cards[0]
+// 	for i := 1; i < 10; i++ {
+// 		s := compFunc(pom, cards[i])
+// 		if s == -1 {
+// 			pom = cards[i]
+// 		}
+// 	}
+// 	return pom
+// }
 
 func main() {
 	//i removed error handling to make the code more readable
-	var Card = []Cards{{5, 2}, {4, 2}, {11, 1}, {11, 2}, {11, 3}, {7, 2}, {13, 2}, {7, 1}, {7, 2}, {5, 1}}
+	var Card = []Cards{{5, 2}, {4, 2}, {11, 1}, {11, 2}, {11, 3}, {7, 2}, {12, 2}, {7, 1}, {7, 2}, {5, 1}}
 
 	//passing as an anonymous function
-	resA := maxA(Card,
+	resA := maxCard(Card,
 		func(Card1 Cards, Card2 Cards) int {
 
 			if Card1.CardVal < Card2.CardVal {
