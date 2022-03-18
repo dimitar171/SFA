117436045964a7c3fc3104ed4175a1b17bfc19af
Dime <dime.jordanov@gmail.com>
2022-03-18 19:38:39 +0100

    correct solution

--- Week4Lecture10/Task1/main.go
+++ Week4Lecture10/Task1/main.go
@@ -8,29 +8,62 @@ import (
 type ConcurrentPrinter struct {
 	sync.WaitGroup
 	sync.Mutex
-}
-
-func (cp *ConcurrentPrinter) printFooBar(times int) {
-
-	for i := 0; i < times; i++ {
-		cp.Add(1)
-		go func() {
-			fmt.Print("foo")
-			cp.Done()
-		}()
-		cp.Wait()
-		cp.Add(1)
-		go func() {
-			fmt.Print("bar")
-			cp.Done()
-		}()
-
-	}
+	counter     int
+	lastElement string
 }
 
 func main() {
-	times := 100
+	times := 10
 	cp := &ConcurrentPrinter{}
-	cp.printFooBar(times)
+	cp.lastElement = "bar"
+	cp.printFoo(times)
+	cp.printBar(times)
 	cp.Wait()
+}
+
+func (cp *ConcurrentPrinter) printFoo(times int) {
+
+	cp.Add(1)
+
+	go func() {
+		defer cp.Done()
+		for {
+			cp.Lock()
+			if cp.counter == times {
+				cp.Unlock()
+				break
+			}
+			if cp.lastElement != "foo" {
+				fmt.Print("foo")
+				cp.lastElement = "foo"
+				cp.counter++
+			}
+			cp.Unlock()
+		}
+
+	}()
+}
+
+func (cp *ConcurrentPrinter) printBar(times int) {
+
+	cp.Add(1)
+
+	go func() {
+		defer cp.Done()
+		for {
+			cp.Lock()
+			if cp.counter == times {
+				cp.Unlock()
+				break
+			}
+			if cp.lastElement != "bar" {
+				fmt.Print("bar")
+				cp.lastElement = "bar"
+				cp.counter++
+			}
+			cp.Unlock()
+		}
+
+	}()
+
 }
